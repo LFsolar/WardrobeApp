8b6aa5eedb1518632cee899bc6442ab1ad590954
LFsolar <sunlisa@gmail.com>
2020-08-01 13:49:42 -0500

    assimilate to orig fork

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from playsound import playsound # play audio
 
 WINDOW_TITLE = "Wardrobe App"
 WINDOW_HEIGHT = 500
-WINDOW_WIDTH = 220
+WINDOW_WIDTH = 200
 IMG_WIDTH = 200
 IMG_HEIGHT = 200
 BEIGE_COLOR_HEX = '#E3C396'
@@ -55,27 +55,58 @@ class WardrobeApp:
 		self.bottom_frame.pack(fill=tk.BOTH, expand=tk.YES)
 
 	def create_buttons(self):
-		top_prev_button = tk.Button(self.tops_frame, text="Prev", command = self.get_next_top)
+		top_prev_button = tk.Button(self.tops_frame, text="Prev", command = self.get_prev_top)
 		top_prev_button.pack(side=tk.LEFT)
 
-		create_outfit_button = tk.Button(self.bottom_frame, text="CREATE OUTFIT", command = self.create_outfit())
+		create_outfit_button = tk.Button(self.bottom_frame, text="Create Outfit", command = self.create_outfit())
 		create_outfit_button.pack(side=tk.LEFT)
 
-		top_next_button = tk.Button(self.tops_frame, text="Next", command = self.get_prev_top)
+		top_next_button = tk.Button(self.tops_frame, text="Next", command = self.get_next_top)
 		top_next_button.pack(side=tk.RIGHT)
 
-		bottom_prev_button = tk.Button(self.bottom_frame, text="Prev", command = self.get_next_bottom)
+		bottom_prev_button = tk.Button(self.bottom_frame, text="Prev", command = self.get_prev_bottom)
 		bottom_prev_button.pack(side=tk.LEFT)
 
-		bottom_next_button = tk.Button(self.bottom_frame, text="Next", command = self.get_prev_bottom)
+		bottom_next_button = tk.Button(self.bottom_frame, text="Next", command = self.get_next_bottom)
 		bottom_next_button.pack(side=tk.RIGHT)
+
+	def create_photo(self, image, frame): # adds an image to window
+		top_image_file = Image.open(image)
+		image = top_image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+		photo = ImageTk.PhotoImage(image) # turn into compatible image
+		image_label = tk.Label(frame, image=photo, anchor=tk.CENTER)
+		#weird tkinter quirk
+		image_label.image = photo
+
+		# so we can add later
+		return image_label
+
+	def update_photo(self, new_image, image_label):
+		# use pillow to change image to something tkinter can understand
+		# collect and change image into tk photo obj
+		new_image_file = Image.open(new_image)
+		image = new_image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+		photo = Image.Tk.PhotoImage(image)
+
+		#update based on provided image label
+		image_label.configure(image=photo)
+
+		# weird tkinter quirk
+		image_label.image = photo
 
 	# generak function that will allow us to move front and back
 	def _get_next_item(self, current_item, category, increment = True):
 
+		""" Gets the Next Item in a Category depending on if you hit next or prev
+		Args:
+			current_item, str
+			category, list
+			increment, boolean
+		"""
+
 		# if we know where the current item index is in a category, then we find the pic before/after it
 		item_index = category.index(current_item)
-		final_index = len(category) -1
+		final_index = len(category) - 1
 		next_index = 0
 
 		# consider the edge cases
@@ -88,10 +119,10 @@ class WardrobeApp:
 		else:
 			# regular up and down
 			# based on increment
-			increment = 1 if increment else -1
-			next_index = item_index + increment
+			incrementor = 1 if increment else -1
+			next_index = item_index + incrementor
 
-		next_image = category.index[next_index]
+		next_image = category[next_index]
 
 		# reset and update the image based on next_image path
 		if current_item in self.top_images:
@@ -104,43 +135,17 @@ class WardrobeApp:
 		# use update function to change the image
 		self.update_image(next_image, image_label)
 
-	def update_image(self, new_image_path, image_label):
-		# use pillow to change image to something tkinter can understand
-		# collect and change image into tk photo obj
-		image_file = Image.open(image_path)
-		image_resized = image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
-		tk_photo = Image.Tk.PhotoImage(image_resized)
-
-		#update based on provided image label
-		image_label.configure(image=tk_photo)
-
-		# weird tkinter quirk
-		image_label.image = tk_photo
-
 	def get_next_top(self):
-		self._get_next_item(self.top_image_path, self.top_images)
+		self._get_next_item(self.top_image_path, self.top_images, increment=True)
 
 	def get_prev_top(self):
 		self._get_next_item(self.top_image_path, self.top_images, increment = False)
 
 	def get_next_bottom(self):
-		self._get_next_item(self.bottom_image_path, self.bottom_images)
+		self._get_next_item(self.bottom_image_path, self.bottom_images, increment=True)
 
 	def get_prev_bottom(self):
 		self._get_next_item(self.bottom_image_path, self.bottom_images, increment = False)
-
-
-
-	def create_photo(self, image_path, frame): # adds an image to window
-		image_file = Image.open(image_path)
-		image_resized = image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
-		tk_photo = ImageTk.PhotoImage(image_resized) # turn into compatible image
-		image_label = tk.Label(frame, image=tk_photo, anchor=tk.CENTER)
-		#weird tkinter quirk
-		image_label.image = tk_photo
-
-		# so we can add later
-		return image_label
 
 	def create_outfit(self):
 
@@ -149,13 +154,14 @@ class WardrobeApp:
 		new_bottom_index = random.randint(0, len(self.bottom_images)-1)
 
 		# add clothes onto screen
-		self.update_image(self.top_images[new_top_index], self.top_image_label)
-		self.update_image(self.bottom_images[new_bottom_index], self.bottom_image_label)
+		self.update_photo(self.top_images[new_top_index], self.top_image_label)
+		self.update_photo(self.bottom_images[new_bottom_index], self.bottom_image_label)
 
 		# add noise
 		playsound(SOUND_EFFECT_FILE_PATH)
 
-root = tk.Tk()
-app = WardrobeApp(root)
-print(ALL_TOPS)
-root.mainloop()
+if __name__ == '__main__':
+	root = tk.Tk()
+	app = WardrobeApp(root)
+
+	root.mainloop()
