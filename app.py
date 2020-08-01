fa4ac31b32a25dab7735a3743129490d2b5a2a85
LFsolar <50917484+LFsolar@users.noreply.github.com>
2020-07-31 23:02:38 -0500

    1st try from youtube

--- app.py
+++ app.py
@@ -1,157 +1,159 @@
 import os
 import random
 
-import tkinter as tk
-from PIL import Image, ImageTk
-from playsound import playsound
+import tkinter as tk # build GUI
+from PIL import Image, ImageTk # manage image
+from playsound import playsound # play audio
 
-# change any of these constants to style and make it your own!
-WINDOW_TITLE = 'Wardrobe App'
+WINDOW_TITLE = "My Wardrobe"
+WINDOW_HEIGHT = 550
 WINDOW_WIDTH = 220
-WINDOW_HEIGHT = 500
-IMG_HEIGHT = 200
-IMG_WIDTH = 200
-BEIGE_COLOR_HEX = '#E3C396'
-SOUND_EFFECT_FILE_PATH = 'assets/yes-2.wav'
-
-# dynamically open folders and make a list, while ignoring any hidden files that start with "."
-# just add any image file into these folders and they will magically appear in your wardrobe!
-# for fun, try to expand this wardrobe to support shoes!
-ALL_TOPS = [str("tops/") + file for file in os.listdir("tops/") if not file.startswith('.')]
-ALL_BOTTOMS = [str("bottoms/") + file for file in os.listdir("bottoms/") if not file.startswith('.')]
-
+IMG_WIDTH = 250
+IMG_HEIGHT = 250
+SOUND_EFFECT_FILE_PATH = 
+# store all the Tops into a file we can access
+ALL_TOPS = [str('tops/') + imagefile for imagefile in os.listdir('tops/') if not imagefile.startswith('.')]
+ALL_BOTTOMS = [str('bottoms/') + imagefile for imagefile in os.listdir('bottoms/') if not imagefile.startswith('.')]
 
 class WardrobeApp:
+	def __init__(self, root):
+		self.root = root
 
-    def __init__(self, root):
-        self.root = root
+		# show top/bottoms image in the window
+		self.top_images = ALL_TOPS
+		self.bottom_images = ALL_BOTTOMS
 
-        # collecting all the clothes
-        self.top_images = ALL_TOPS
-        self.bottom_images = ALL_BOTTOMS
+		# save single top & bottom
+		self.top_image_path = self.top_images[0]
+		self.bottom_image_path = self.bottom_images[0]
 
-        # first pictures for top and bottom
-        self.tops_image_path = self.top_images[0]
-        self.bottom_image_path = self.bottom_images[0]
+		# pack img into top screen, then pack top screen into root
+		# create and add top & bottom image into Frame
+		self.tops_frame = tk.Frame(self.root)		
+		self.bottom_frame = tk.Frame(self.root)		
+		self.top_image_label = self.create_photo(self.top_image_path, self.tops_frame)
+		self.bottom_image_label = self.create_photo(self.bottom_image_path, self.bottom_frame)
+		# add it to pack
+		self.top_image_label.pack(side=tk.TOP)
+		self.bottom_image_label.pack(side=tk.TOP)
 
-        # creating 2 frames
-        self.tops_frame = tk.Frame(self.root, bg=BEIGE_COLOR_HEX)
-        self.bottoms_frame = tk.Frame(self.root, bg=BEIGE_COLOR_HEX)
+		# create background
+		self.create_background()
 
-        # adding top
-        self.top_image_label = self.create_photo(self.tops_image_path, self.tops_frame)
-        self.top_image_label.pack(side=tk.TOP)
+	def create_background(self):
+		# add title to window and change the size
+		self.root.title(WINDOW_TITLE)
+		self.root.geometry('{0}x{1}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
 
-        # adding bottom
-        self.bottom_image_label = self.create_photo(self.bottom_image_path, self.bottoms_frame)
-        self.bottom_image_label.pack(side=tk.TOP)
+		# add all buttons
+		self.create_buttons()
 
-        self.create_background()
+		# add clothing
+		self.tops_frame.pack(fill=tk.BOTH, expand=tk.YES)
+		self.bottom_frame.pack(fill=tk.BOTH, expand=tk.YES)
 
-    def create_background(self):
-        # title and resize the window
-        self.root.title(WINDOW_TITLE)
-        self.root.geometry('{0}x{1}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
+	def create_buttons(self):
+		top_prev_button = tk.Button(self.tops_frame, text="Prev", command = self.get_next_top)
+		top_prev_button.pack(side=tk.LEFT)
 
-        # create buttons
-        self.create_buttons()
+		create_outfit_button = tk.Button(self.bottom_frame, text="CREATE OUTFIT", command = self.create_outfit())
+		create_outfit_button.pack(side=tk.LEFT)
 
-        # add the initial clothes onto the screen
-        self.tops_frame.pack(fill=tk.BOTH, expand=tk.YES)
-        self.bottoms_frame.pack(fill=tk.BOTH, expand=tk.YES)
+		top_next_button = tk.Button(self.tops_frame, text="Next", command = self.get_prev_top)
+		top_next_button.pack(side=tk.RIGHT)
 
-    def create_buttons(self):
-        top_prev_button = tk.Button(self.tops_frame, text="Prev", command=self.get_prev_top)
-        top_prev_button.pack(side=tk.LEFT)
+		bottom_prev_button = tk.Button(self.bottom_frame, text="Prev", command = self.get_next_bottom)
+		bottom_prev_button.pack(side=tk.LEFT)
 
-        create_outfit_button = tk.Button(self.tops_frame, text="Create Outfit", command=self.create_outfit)
-        create_outfit_button.pack(side=tk.LEFT)
+		bottom_next_button = tk.Button(self.bottom_frame, text="Next", command = self.get_prev_bottom)
+		bottom_next_button.pack(side=tk.RIGHT)
 
-        top_next_button = tk.Button(self.tops_frame, text="Next", command=self.get_next_top)
-        top_next_button.pack(side=tk.RIGHT)
+	# generak function that will allow us to move front and back
+	def _get_next_item(self, current_item, category, increment = True):
 
-        bottom_prev_button = tk.Button(self.bottoms_frame, text="Prev", command=self.get_prev_bottom)
-        bottom_prev_button.pack(side=tk.LEFT)
+		# if we know where the current item index is in a category, then we find the pic before/after it
+		item_index = category.index(current_item)
+		final_index = len(category) -1
+		next_index = 0
 
-        bottom_next_button = tk.Button(self.bottoms_frame, text="Next", command=self.get_next_bottom)
-        bottom_next_button.pack(side=tk.RIGHT)
+		# consider the edge cases
+		if increment and item_index == final_index:
+			# add the end, and need to up, cycle back to beginning
+			next_index = 0
+		elif not increment and item_index == 0:
+			# cycle back to end
+			next_index = final_index
+		else:
+			# regular up and down
+			# based on increment
+			increment = 1 if increment else -1
+			next_index = item_index + increment
 
-    def create_photo(self, image, frame):
-        top_image_file = Image.open(image)
-        image = top_image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
-        photo = ImageTk.PhotoImage(image)
-        image_label = tk.Label(frame, image=photo, anchor=tk.CENTER)
-        image_label.image = photo
+		next_image = category.index[next_index]
 
-        return image_label
+		# reset and update the image based on next_image path
+		if current_item in self.top_images:
+			image_label = self.top_image_label
+			self.top_image_path = next_image
+		else:
+			image_label = self.bottom_image_label
+			self.bottom_image_path = next_image			
 
-    def update_photo(self, new_image, image_label):
-        new_image_file = Image.open(new_image)
-        image = new_image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
-        photo = ImageTk.PhotoImage(image)
-        image_label.configure(image=photo)
-        image_label.image = photo
+		# use update function to change the image
+		self.update_image(next_image, image_label)
 
-    def _get_next_item(self, current_item, category, increment=True):
-        """ Gets the Next Item In a Category depending on if you hit next or prev
+	def update_image(self, new_image_path, image_label):
+		# use pillow to change image to something tkinter can understand
+		# collect and change image into tk photo obj
+		image_file = Image.open(image_path)
+		image_resized = image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+		tk_photo = Image.Tk.PhotoImage(image_resized)
 
-        Args:
-            current_item, str
-            category, list
-            increment, boolean
-        """
-        item_index = category.index(current_item)
-        final_index = len(category) - 1
-        next_index = 0
+		#update based on provided image label
+		image_label.configure(image=tk_photo)
 
-        if increment and item_index == final_index:
-            next_index = 0  # cycle back to the beginning
-        elif not increment and item_index == 0:
-            next_index = final_index  # cycle back to the end
-        else:
-            incrementor = 1 if increment else -1
-            next_index = item_index + incrementor
+		# weird tkinter quirk
+		image_label.image = tk_photo
 
-        next_image = category[next_index]
+	def get_next_top(self):
+		self._get_next_item(self.top_image_path, self.top_images)
 
-        # reset the image object
-        if current_item in self.top_images:
-            image_label = self.top_image_label
-            self.tops_image_path = next_image
-        else:
-            image_label = self.bottom_image_label
-            self.bottom_image_path = next_image
+	def get_prev_top(self):
+		self._get_next_item(self.top_image_path, self.top_images, increment = False)
 
-        # update the photo
-        self.update_photo(next_image, image_label)
+	def get_next_bottom(self):
+		self._get_next_item(self.bottom_image_path, self.bottom_images)
 
-    def get_next_top(self):
-        self._get_next_item(self.tops_image_path, self.top_images, increment=True)
-
-    def get_prev_top(self):
-        self._get_next_item(self.tops_image_path, self.top_images, increment=False)
-
-    def get_prev_bottom(self):
-        self._get_next_item(self.bottom_image_path, self.bottom_images, increment=False)
-
-    def get_next_bottom(self):
-        self._get_next_item(self.bottom_image_path, self.bottom_images, increment=True)
-
-    def create_outfit(self):
-        # randomly select an outfit
-        new_top_index = random.randint(0, len(self.top_images)-1)
-        new_bottom_index = random.randint(0, len(self.bottom_images)-1)
-
-        # add the clothes onto the screen
-        self.update_photo(self.top_images[new_top_index], self.top_image_label)
-        self.update_photo(self.bottom_images[new_bottom_index], self.bottom_image_label)
-
-        # spicy noise
-        playsound(SOUND_EFFECT_FILE_PATH)
+	def get_prev_bottom(self):
+		self._get_next_item(self.bottom_image_path, self.bottom_images, increment = False)
 
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = WardrobeApp(root)
 
-    root.mainloop()
+	def create_photo(self, image_path, frame): # adds an image to window
+		image_file = Image.open(image_path)
+		image_resized = image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+		tk_photo = ImageTk.PhotoImage(image_resized) # turn into compatible image
+		image_label = tk.Label(frame, image=tk_photo, anchor=tk.CENTER)
+		#weird tkinter quirk
+		image_label.image = tk_photo
+
+		# so we can add later
+		return image_label
+
+	def create_outfit(self):
+
+		# rand select a top & bottom index
+		new_top_index = random.randint(0, len(self.top_images)-1)
+		new_bottom_index = random.randint(0, len(self.bottom_images)-1)
+
+		# add clothes onto screen
+		self.update_image(self.top_images[new_top_index], self.top_image_label)
+		self.update_image(self.bottom_images[new_bottom_index], self.bottom_image_label)
+
+		# add noise
+		playsound(SOUND_EFFECT_FILE_PATH)
+
+root = tk.Tk()
+app = WardrobeApp(root)
+print(ALL_TOPS)
+root.mainloop()
